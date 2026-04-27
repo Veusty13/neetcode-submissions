278193bd6916745f2bb943ddb2a98988f3a6d165
Steve RASSINOT <51273552+Veusty13@users.noreply.github.com>
2026-04-27 22:11:14 +0200

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        def dfs(nb_climbed_stairs, target):
+            if nb_climbed_stairs == target:
+                return 1
+            if nb_climbed_stairs > target:
+                return 0
+            if nb_climbed_stairs not in memo:
+                memo[nb_climbed_stairs] = dfs(nb_climbed_stairs + 1, target) + dfs(nb_climbed_stairs + 2, target)
+            return memo[nb_climbed_stairs]
+        return dfs(0, n) 
