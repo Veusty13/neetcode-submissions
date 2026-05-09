c878cee75ecb31357689c47846893e907632827c
Steve RASSINOT <51273552+Veusty13@users.noreply.github.com>
2026-05-09 21:10:50 +0200

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.py
@@ -0,0 +1,23 @@
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        sep = str("{size}#")
+        encoded = ""
+        for s in strs:
+            size = len(s)
+            encoded += (sep.format(size=size) + s)
+        return encoded
+    def decode(self, s: str) -> List[str]:
+        r = 0
+        size_as_string = ""
+        n = len(s)
+        decoded = []
+        while r < n:
+            while s[r] != "#":
+                size_as_string += s[r]
+                r += 1
+            size = int(size_as_string)
+            decoded.append(s[r + 1: r + size + 1])
+            size_as_string = ""
+            r = r + size + 1
+        return decoded
+
